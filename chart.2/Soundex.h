258ebcc089f3ce7567259c057ec3e74e0f7e012c
wutong <wutong@megvii.com>
2022-03-31 17:03:46 +0800

    最终重构版本

--- chart.2/Soundex.h
+++ chart.2/Soundex.h
@@ -2,13 +2,29 @@
 #define _SOUNDEX_H_
 #include <string>
 #include <unordered_map>
+#include <cctype>
 
-class Soundex {
+class Soundex{
     static const size_t MaxCodeLength{4};
     const std::string NotADigit{ "*" };
+
 public:
     std::string encode(const std::string & word) const{
         return zeroPad(upperFront(head(word)) + tail(encodedDigits(word)));
+    }
+
+    std::string encodedDigits(char letter) const{
+        const std::unordered_map<char, std::string> encodings{
+            { 'b', "1" }, { 'f', "1" }, { 'p', "1" }, { 'v', "1" },
+            { 'c', "2" }, { 'g', "2" }, { 'j', "2" }, { 'k', "2" },
+            { 'q', "2" }, { 's', "2" }, { 'x', "2" }, { 'z', "2" },
+            { 'd', "3" }, { 't', "3" },
+            { 'l', "4" },
+            { 'm', "5" }, { 'n', "5" },
+            { 'r', "6" }
+        };
+        auto it = encodings.find(tolower(letter));
+        return it == encodings.end() ? NotADigit : it->second;
     }
 
 private:
@@ -20,7 +36,7 @@ private:
         return std::tolower(static_cast<unsigned char>(c));
     }
 
-    std::string head(const std::string & word) const {
+    std::string head(const std::string & word) const{
         return word.substr(0, 1);
     }
 
@@ -28,38 +44,33 @@ private:
         return word.substr(1);
     }
 
-    std::string encodedDigits(const std::string & word) const {
-        std::string encoding;
+    void encodeHead(std::string & encoding, const std::string & word) const{
+        encoding += encodedDigits(word.front());
+    }
 
-        encoding += encodedDigit(word.front());
+    void encodeLetter(std::string & encoding, char letter, char lastLetter) const{
+        auto digit = encodedDigits(letter);
+        if (digit != NotADigit && 
+            (digit != lastDigit(encoding) || isVowel(lastLetter)))
+            encoding += digit;
+    }
 
-        for (auto letter: tail(word)) {
-            if (isComplete(encoding)) break;
+    bool isVowel(char letter) const{
+        return std::string("aeiouy").find(lower(letter)) != std::string::npos;
+    }
 
-            auto digit = encodedDigit(letter);
-            if (digit != NotADigit && digit != lastDigit(encoding))
-                encoding += digit;
+    void encodeTail(std::string & encoding, const std::string & word) const{
+        for (auto i = 1u; i < word.length(); i++){
+            if (!isComplete(encoding))
+                encodeLetter(encoding, word[i], word[i - 1]);           
         }
+    }
+
+    std::string encodedDigits(const std::string & word) const{
+        std::string encoding;
+        encodeHead(encoding, word);
+        encodeTail(encoding, word);                 
         return encoding;
-    }
-
-    std::string encodedDigit(char letter) const {
-        const std::unordered_map<char, std::string> encodings {
-            { 'b', "1" }, { 'f', "1" }, { 'p', "1" }, { 'v', "1" },
-            { 'c', "2" }, { 'g', "2" }, { 'j', "2" }, { 'k', "2" },
-            { 'q', "2" }, { 's', "2" }, { 'x', "2" }, { 'z', "2" },
-            { 'd', "3" }, { 't', "3" },
-            { 'l', "4" },
-            { 'm', "5" }, { 'n', "5" },
-            { 'r', "6" }
-        };
-        auto it = encodings.find(lower(letter));
-        return it == encodings.end() ? "": it->second;
-    }
-
-    std::string zeroPad(const std::string& word) const {
-        auto zerosNeeded = MaxCodeLength - word.length();
-        return word + std::string(zerosNeeded, '0');
     }
 
     std::string lastDigit(const std::string & encoding) const{
@@ -70,6 +81,11 @@ private:
     bool isComplete(const std::string & encoding) const{
         return encoding.length() == MaxCodeLength;
     }
+
+    std::string zeroPad(const std::string & word) const{
+        auto zerosNeed = MaxCodeLength - word.length();
+        return word + std::string(zerosNeed, '0');
+    }
 };
 
-#endif
+#endif // _SOUNDEX_H_
--- chart.2/SoundexTest.cpp
+++ chart.2/SoundexTest.cpp
@@ -1,38 +1,62 @@
 #include <gmock/gmock.h>
-#include <string>
 #include "Soundex.h"
-using namespace std;
 using namespace testing;
 
-class SoundexEncoding: public Test {
+class SoundexEncoding :public Test{
 public:
     Soundex soundex;
 };
 
 TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord){
-    auto encoded = soundex.encode("A");
-    ASSERT_EQ(encoded, std::string("A000"));
+    ASSERT_THAT(soundex.encode("A"), Eq("A000"));
 }
 
 TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits){
-    auto encoded = soundex.encode("I");
-    ASSERT_EQ(encoded, std::string("I000"));
+    ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
 
 TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateDigits){
+    ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics){
     ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits){
+    ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
+
+TEST_F(SoundexEncoding, LimitsLengthToFourCharacters){
+    ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters){
+    ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings){
+    ASSERT_THAT(soundex.encodedDigits('b'), soundex.encodedDigits('f'));
+    ASSERT_THAT(soundex.encodedDigits('c'), soundex.encodedDigits('g'));
+    ASSERT_THAT(soundex.encodedDigits('d'), soundex.encodedDigits('t'));
+
+    ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
 }
 
 TEST_F(SoundexEncoding, UppercasesFirstLetter){
     ASSERT_THAT(soundex.encode("abcd"), StartsWith("A"));
 }
 
-TEST_F(SoundexEncoding, IgnoreVowelLikeLetters){
-    ASSERT_THAT(soundex.encode("BaAeEiIoOuUhHyYcdl"), Eq("B234"));
+TEST_F(SoundexEncoding, IgnoresCaseWhenEncodingConsonants){
+    ASSERT_THAT(soundex.encode("BCDL"), soundex.encode("Bcdl"));
 }
 
-TEST_F(SoundexEncoding, IgnoresCaseWhenEncodingConsonants){
-    ASSERT_THAT(soundex.encode("BCDL"), Eq(soundex.encode("Bcdl")));
+TEST_F(SoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicateslst){
+    ASSERT_THAT(soundex.encode("Bbcd"), Eq("B230"));
+}
+
+TEST_F(SoundexEncoding, DoesNotCombineDuplicateEncodingsSeparatedByVowels){
+    ASSERT_THAT(soundex.encode("Jbob"), Eq("J110"));
 }
 
 int main(int argc, char** argv) {
